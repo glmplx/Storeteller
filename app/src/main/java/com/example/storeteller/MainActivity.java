9050dbfa015029f6e7e79f14e28ff589b006e74f
Guillaume <guillaume.pailloux@hotmail.fr>
2023-10-17 16:10:41 +0200

    Button + Hello World

--- app/src/main/java/com/example/storeteller/MainActivity.java
+++ app/src/main/java/com/example/storeteller/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.storeteller;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -32,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Trouver le bouton par son ID
+        Button button = findViewById(R.id.button);
+
+        // Ajouter un OnClickListener au bouton
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Afficher un toast "Hello World"
+                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 
 }
--- app/src/main/java/com/example/storeteller/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/storeteller/ui/dashboard/DashboardFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.storeteller.MainActivity;
+import com.example.storeteller.R;
 import com.example.storeteller.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
